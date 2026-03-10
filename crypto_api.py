deb7811b584d047317beb240d28f4cffbc7dc032
Moizuddin Siddiqui <moizuddinsiddiqui26@gmail.com>
2026-03-11 01:14:49 +0530

    Refactor fetch_crypto_prices to fetch_data and remove API_KEY

--- crypto_api.py
+++ crypto_api.py
@@ -1,21 +1,12 @@
-import requests
-import pandas as pd
+import requests, pandas as pd
 
-API_KEY="CG-HdRxpnS5T616DTh22gcRGyRG"
-
-def fetch_crypto_prices():
-
+def fetch_data():
     ids="bitcoin,ethereum,binancecoin,cardano,solana"
-
-    url=f"https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids={ids}&x_cg_demo_api_key={API_KEY}"
-
+    url=f"https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids={ids}"
     res=requests.get(url).json()
 
     data=[]
-    for coin in res:
-        data.append({
-            "Crypto":coin.capitalize(),
-            "Price":res[coin]["usd"]
-        })
+    for c in res:
+        data.append({"Crypto":c.capitalize(),"Price":res[c]["usd"]})
 
     return pd.DataFrame(data)
