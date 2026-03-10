e1c613240a019c61714456323f834c5389289431
Moizuddin Siddiqui <moizuddinsiddiqui26@gmail.com>
2026-03-10 22:59:01 +0530

    Add email alert functionality for high risk alerts

---
+++ email_alert.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+SENDER="managercryptoportfolio@gmail.com"
+APP_PASSWORD="YOUR_APP_PASSWORD"
+
+def send_alert(df):
+
+    body=f"""
+High Risk Alert!
+
+{df.to_string()}
+"""
+
+    msg=MIMEText(body)
+    msg["Subject"]="Crypto Risk Alert"
+    msg["From"]=SENDER
+    msg["To"]=SENDER
+
+    with smtplib.SMTP_SSL("smtp.gmail.com",465) as server:
+        server.login(SENDER,APP_PASSWORD)
+        server.send_message(msg)
